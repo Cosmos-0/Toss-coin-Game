fc7e8138e41befb760d341650e0c71e3530da4d4
Cosmos-0 <gamek909@gmail.com>
2023-11-17 03:37:25 +0100

    fixing the bug where only the heads displays in both cases

--- src/CoinToss.jsx
+++ src/CoinToss.jsx
@@ -1,14 +1,11 @@
 import React, { useState } from 'react';
 import './CoinToss.css'; // Ensure this CSS file is updated as well
-import headsImage from './5dh-sowa-miw.png'; // Adjust the path as per your directory structure
-import tailsImage from './5dh-terma-miw.png';
-
+import headsImage from './5dh-sowa-miw.png'; // Path to heads image
+import tailsImage from './5dh-terma-miw.png'; // Path to tails image
 
 const CoinToss = () => {
   const [result, setResult] = useState('');
   const [flipping, setFlipping] = useState(false);
-
-
 
   const tossCoin = () => {
     setFlipping(true);
@@ -20,12 +17,14 @@ const CoinToss = () => {
   };
 
   return (
-    <div className="coin-toss-container" >
+    <div className="coin-toss-container">
       <h1>"Sbe3" wela "Malik"</h1>
-      <div className={`coin ${flipping ? 'flip' : ''} `}>
-      <img 
-      class="h-2"
-      src={result === 'Heads' ? headsImage : tailsImage} alt={result} />
+      <div className={`coin ${flipping ? 'flip' : ''}`}>
+        <img
+          className="h-2"
+          src={result === 'Sbe3' ? headsImage : tailsImage}
+          alt={result ? result : 'Coin'}
+        />
       </div>
       <button onClick={tossCoin}>gleb</button>
       {result && <div className="result">Result: {result}</div>}
